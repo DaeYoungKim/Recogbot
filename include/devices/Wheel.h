16e5b2475baa8b2383f2054343b3293e27184b0b
super373 <super373@gmail.com>
2011-08-15 12:32:21 +0900

    Hello Wheel message added

---
+++ include/devices/Wheel.h
@@ -0,0 +1,17 @@
+/*
+	CNU, IIMIL
+	Guochao Chang
+
+	Created in August, 2011 by Guochao Chang(super373@gmail.com)
+*/
+
+#ifndef RECOGBOT_DEVICES_Wheel_H
+#define RECOGBOT_DEVICES_Wheel_H
+
+namespace Recogbot {
+	class Wheel {
+	public:
+		Wheel();
+	};
+}
+#endif
---
+++ src/devices/Wheel.cpp
@@ -0,0 +1,8 @@
+#include "devices/Wheel.h"
+#include <cstdio>
+
+namespace Recogbot {
+	Wheel::Wheel(){
+		printf("Hell Wheel\n");
+	}
+}
