d6d7a67b5cc6b2ef68eb40e2e7a4e2530d31a90b
Chi-Min Oh <sapeyes@gmail.com>
2011-08-15 14:02:33 +0900

    deleted unnecessary files

--- examples/OmniCameraTest.cpp
+++ examples/OmniCameraTest.cpp
@@ -1,26 +1,12 @@
 #include "devices/OmniCamera.h"
 #include <iostream>
 #include "opencv2/opencv.hpp"
-#include "..\include\devices\Psd.h"
-#include "devices/Wheel.h"
 
 using namespace Recogbot;
 using namespace std;
-<<<<<<< HEAD
 
 void main(void) {
 
-	Wheel::Wheel();
-	system("pause");
-	return;
-
-	PSD psd;
-	return;
-=======
-#include "devices/Psd.h"
-
-void main(void) {
->>>>>>> chiminoh/master
 	OmniCamera omnicam;
 	cout << omnicam.connect();
 	IplImage *frame=cvCreateImage(cvSize(omnicam.getWidth(),omnicam.getHeight()),8,3);
